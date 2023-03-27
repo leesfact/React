01eb31d52463d4882e3dd89dafb0afe84ab4540e
leesfact <lky110408@gmail.com>
2023-03-27 14:16:38 +0900

    요구사항 실습 전 백업

--- react_study/src/pages/UserList/UserList.js
+++ react_study/src/pages/UserList/UserList.js
@@ -12,9 +12,12 @@ const UserList = () => {
         username: '',
         password: '',
         name: '',
-        email: ''
+        email: '',
+        modifyFlag: false
     }
 
+    const userIndex = useRef(1);
+    const [users, setUsers] = useState([]);
     const [inputs, setInputs] = useState(user);
     const inputRefs = [useRef(), useRef(), useRef(), useRef()];
     const addButtonRef = useRef();
@@ -40,42 +43,55 @@ const UserList = () => {
     }
 
     const addHandler = () => {
+        const user = {
+            ...inputs
+        };
 
+        user.id = userIndex.current++;
+
+        setUsers([...users, user]);
     }
 
-    const users = [
-        {
-            id: 1,
-            username: 'aaa',
-            password: '1234',
-            name: 'AAA',
-            email: 'aaa@gmail.com'
-        },
-        {
-            id: 2,
-            username: 'bbb',
-            password: '1234',
-            name: 'BBB',
-            email: 'bbb@gmail.com'
-        },
-        {
-            id: 3,
-            username: 'ccc',
-            password: '1234',
-            name: 'CCC',
-            email: 'ccc@gmail.com'
-        },
-    ]
+    const onRemove = (index) => {
+        // users.splice(index - 1, 1);
+        // setUsers([...users]);
+        
+        setUsers(users.filter(user => user.id !== index));
+    }
 
-    const userIndex = useRef(4);
+    const onModify = (index) => {
+        setUsers(users.map(user => {
+            if(user.id === index) {
+                setInputs({...user});
+                user.modifyFlag = true;
+            }else {
+                user.modifyFlag = false;
+            }
+            return user;
+        }));
+    }
+
+    const onSave = (index) => {
+        setUsers(users.map(user => {
+            if(user.id === index) {
+                user.modifyFlag = true;
+                return {
+                    ...inputs,
+                    id: user.id
+                };
+            }
+            return user;
+        }));
+    }
+
 
     return (
         <div css={S.Container}>
             <div>
-                <input type="text" onKeyUp={keyupHandler} onChange={inputHandler} placeholder='username' name='username' value={0} ref={inputRefs[0]}/>
-                <input type="text" onKeyUp={keyupHandler} onChange={inputHandler} placeholder='password' name='password' value={0} ref={inputRefs[1]}/>
-                <input type="text" onKeyUp={keyupHandler} onChange={inputHandler} placeholder='name' name='name' value={0} ref={inputRefs[2]}/>
-                <input type="text" onKeyUp={keyupHandler} onChange={inputHandler} placeholder='email' name='email' value={0} ref={inputRefs[3]}/>
+                <input type="text" onKeyUp={keyupHandler} onChange={inputHandler} placeholder='username' name='username' ref={inputRefs[0]}/>
+                <input type="text" onKeyUp={keyupHandler} onChange={inputHandler} placeholder='password' name='password' ref={inputRefs[1]}/>
+                <input type="text" onKeyUp={keyupHandler} onChange={inputHandler} placeholder='name' name='name' ref={inputRefs[2]}/>
+                <input type="text" onKeyUp={keyupHandler} onChange={inputHandler} placeholder='email' name='email' ref={inputRefs[3]}/>
                 <button type='button' onClick={addHandler} ref={addButtonRef}>추가</button>
             </div>
             <table css={S.Table}>
@@ -86,18 +102,29 @@ const UserList = () => {
                         <th css={S.ThAndTd}>password</th>
                         <th css={S.ThAndTd}>name</th>
                         <th css={S.ThAndTd}>email</th>
+                        <th css={S.ThAndTd}>update</th>
+                        <th css={S.ThAndTd}>delete</th>
                     </tr>
                 </thead>
                 <tbody>
                     {users.map(user => {
                         
                         return (
-                            <tr>
+                            <tr key={user.id}>
                                 <td css={S.ThAndTd}>{user.id}</td>
-                                <td css={S.ThAndTd}>{user.username}</td>
-                                <td css={S.ThAndTd}>{user.password}</td>
-                                <td css={S.ThAndTd}>{user.name}</td>
-                                <td css={S.ThAndTd}>{user.email}</td>
+                                <td css={S.ThAndTd}>{user.modifyFlag ? (<input type="text" onKeyUp={keyupHandler} onChange={inputHandler} placeholder='username' name='username' ref={inputRefs[0]} defaultValue={user.username} />) : user.username}</td>
+                                <td css={S.ThAndTd}>{user.modifyFlag ? (<input type="text" onKeyUp={keyupHandler} onChange={inputHandler} placeholder='password' name='password' ref={inputRefs[1]} defaultValue={user.password} />) : user.password}</td>
+                                <td css={S.ThAndTd}>{user.modifyFlag ? (<input type="text" onKeyUp={keyupHandler} onChange={inputHandler} placeholder='name' name='name' ref={inputRefs[2]} defaultValue={user.name} />) : user.name}</td>
+                                <td css={S.ThAndTd}>{user.modifyFlag ? (<input type="text" onKeyUp={keyupHandler} onChange={inputHandler} placeholder='email' name='email' ref={inputRefs[3]} defaultValue={user.email} />) : user.email}</td>
+                                <td css={S.ThAndTd}>
+                                    {user.modifyFlag 
+                                        ? (<button onClick={() => onSave(user.id)}>확인</button>)
+                                        : (<button onClick={() => onModify(user.id)}>수정</button>) 
+                                    }
+                                </td>
+                                <td css={S.ThAndTd}>
+                                    <button onClick={() => onRemove(user.id)}>삭제</button>    
+                                </td>
                             </tr>
                         );
                     })}
